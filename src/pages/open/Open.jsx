7b930b91ae6fee18ac50dbe03eb289da80664694
Gil <gil190267@gmail.com>
2025-09-21 09:17:58 +0300

    Display in open page git info from back + front

--- src/pages/open/Open.jsx
+++ src/pages/open/Open.jsx
@@ -1,19 +1,24 @@
 import './Open.css'
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useLocation } from 'wouter'
 import { useDeviceResolution } from '../../contexts/DeviceResolution';
 import { db } from '../../api/db';
+import gitInfo from '../../../git_info.json';
 
 function OpenPage() {
     const [_, navigate ] = useLocation();
     const { deviceType } = useDeviceResolution();
     const [msg, setMsg] = useState('');
+    const [viewGitInfo, setViewGitInfo] = useState(false);
+    const [backendGitInfo, setBackendGitInfo] = useState({branch:"", commit:""});
+    const openTimer = useRef();
 
     async function dbAvailable() {
         const result = await db.available();
 
         if (result.success) {
-            setTimeout(() => {
+            setBackendGitInfo(result.git);
+            openTimer.current = setTimeout(() => {
                 navigate('/admin');
             }, 3000)
         }
@@ -23,18 +28,50 @@ function OpenPage() {
         }
     }
 
+    function titleClick() {
+        if (viewGitInfo)
+            navigate('/admin');
+        else {
+            clearTimeout(openTimer.current);
+            setViewGitInfo(true);
+        }
+    }
+
     useEffect(() => {
         dbAvailable();
     }, [])
 
     return (
         <div className={`open-page ${deviceType}`}>
-            <div className="title">
+            <div className="title" onClick={titleClick}>
                 <h1>מיידעון</h1>
             </div>
             {
                 msg &&
                 <h3>{msg}</h3>
+            }
+            {
+                viewGitInfo &&
+                <div className='git-info'><b>
+                    <div>
+                        Backend:
+                    </div>
+                    <div>
+                        {`branch: ${backendGitInfo.branch}`}
+                    </div>
+                    <div>
+                        {`commit: ${backendGitInfo.commit}`}
+                    </div>
+                    <div>
+                        Frotend:
+                    </div>
+                    <div>
+                        {`branch: ${gitInfo.branch}`}
+                    </div>
+                    <div>
+                        {`commit: ${gitInfo.commit}`}
+                    </div>
+                </b></div>
             }
         </div>
     )
