d4e73d635e3cbda644ffb1ea7d1190e65dfd55f9
eli-pavlov <pavlov.eli@gmail.com>
2025-10-09 14:47:36 +0300

    update

--- src/pages/home/components/Header.jsx
+++ src/pages/home/components/Header.jsx
@@ -3,30 +3,34 @@ import { useSettingsContext } from '../../../contexts/SettingsContext.jsx';
 import { useDeviceContext } from '../../../contexts/DeviceResolution.jsx';
 import { useLocation } from 'wouter';
 import { useAuthContext } from '../../../contexts/AuthContext.jsx';
-// FIX: Using the correct function and constant names from cookies.js
+// FIX: Using the correct function and constant names
 import { removeCookie, AUTH_USER } from '../../../utils/cookies.js';
 
 function Header() {
     const { settings } = useSettingsContext();
     const { deviceType } = useDeviceContext();
     const [_, setLocation] = useLocation();
-    const { setUser } = useAuthContext();
+    const { user, setUser } = useAuthContext();
 
     function handleLogout() {
         setUser(null);
-        // FIX: Using the correct function and constant names
+        // FIX: Using the correct function and constant
         removeCookie(AUTH_USER);
         setLocation('/login');
     }
 
     return (
         <div className={`header ${deviceType}`}>
-            <div className={`admin-icon ${deviceType}`} onClick={() => setLocation('/admin')}>
-                <i className="fa fa-gear"></i>
-            </div>
-            <div className={`logout-icon ${deviceType}`} onClick={handleLogout}>
-                <i className="fa fa-sign-out"></i>
-            </div>
+            {user && (
+                <>
+                    <div className={`admin-icon ${deviceType}`} onClick={() => setLocation('/admin')}>
+                        <i className="fa fa-gear"></i>
+                    </div>
+                    <div className={`logout-icon ${deviceType}`} onClick={handleLogout}>
+                        <i className="fa fa-sign-out"></i>
+                    </div>
+                </>
+            )}
             <div>{settings.title}</div>
         </div>
     );
